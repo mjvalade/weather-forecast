d11bdf4524ed6c28aa5041c8e5a3ac70d3e1f2a9
Peter Springer <peterspringer829@gmail.com>
2016-10-11 09:04:27 -0600

    pinned citys component

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-const CityCard = ({pinnedCurrentForecast}) => {
-  console.log(pinnedCurrentForecast);
-  console.log(this.props.zipcode);
-  // this.props.fetchCurrentWeatherByZip(80021);
-  
-    return (
-      <section className="city-card">
-        {Object.keys(pinnedCurrentForecast).length > 0 ?
-        <article>
-          <h1 className="city-title">{pinnedCurrentForecast.name}</h1>
-          <ul className="city-list">
-            <li className="city-li">Current temp: {pinnedCurrentForecast.main.temp}&#8457;</li>
-            <li className="city-li">Condition: {pinnedCurrentForecast.weather[0].description}</li>
-          </ul>
-        </article>
-        : <p>Fetching pinned city forecast ...</p>}
+export default class CityCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipcode: this.props.zipcode,
+      currentTemp: null,
+      condition: null,
+      cityName: null
+    };
+  }
+  fetchCurrentWeatherByZip(zipcode) {
+      const apiKey = 'cbc43ed2ea5ef4a7aa9e8cf85994a583';
+      let weatherURLbyZip = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&appid=${apiKey}`;
+          return fetch(weatherURLbyZip)
+              .then(response => response.json())
+              .then(weatherByZip => this.setState({
+                currentTemp: Math.round(weatherByZip.main.temp),
+                condition: weatherByZip.weather[0].main,
+                cityName: weatherByZip.name
+              }));
+      };
+
+      componentDidMount() {
+        this.fetchCurrentWeatherByZip(this.state.zipcode)
+      }
+render() {
+  return (
+    <section className="city-card">
         <article className="pinned-forecast-extended">
           <Link to="/ExtendedForecast"
-          // add route to extended forecast for particular zipcode
+            // add route to extended forecast for particular zipcode
             className="pinned-link">
             View Extended Forecast &#10163;
           </Link>
         </article>
       </section>
     );
-};
-
-export default CityCard;
+  };
+}
